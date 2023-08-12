e1520215e208aa6b761497e0934b45f0250268bf
EVLG <everlastingspring62@gmail.com>
2023-08-12 16:47:51 +1000

    Add API to return prediction for single photo

---
+++ python_app/common/image_prediction_result.py
@@ -0,0 +1,7 @@
+class imagePredictionResult:
+    def __init__(self, plantName, plantDescription, confidence):
+        self.plantName = plantName
+        self.plantDescription = plantDescription
+        self.confidence = confidence
+
+    
--- python_app/infrastructure/storage_service.py
+++ python_app/infrastructure/storage_service.py
@@ -1,11 +1,15 @@
 import tensorflow as tf
 import os.path
 import pathlib
-
+from PIL import Image
+import io
+import uuid
 class storageService:
+	def __init__(self):
+		self.img_height= 180
+		self.img_width = 180
+
 	def retrieveMockImages(self):
-		img_height = 180
-		img_width = 180
 		img_dict = dict()
 		dataset_path = os.path.abspath(os.path.dirname("") + "mock_photos")
 		for image in os.listdir(dataset_path):
@@ -13,7 +17,16 @@ class storageService:
 			print(img_path)
 			data_dir = pathlib.Path(tf.keras.utils.get_file(img_path, origin=''))
 			img = tf.keras.utils.load_img(
-				data_dir, target_size=(img_height, img_width)
+				data_dir, target_size=(self.img_height, self.img_width)
 			)
 			img_dict[image] = tf.expand_dims(tf.keras.utils.img_to_array(img), 0)
+		return img_dict
+	
+	def tensoriseImage(self, img_bytes):
+		img_dict = dict()
+		imageName = uuid.uuid4().hex
+		img = Image.open(io.BytesIO(img_bytes))
+		img = img.convert('RGB')
+		img = img.resize(size=(self.img_height, self.img_width), resample=Image.NEAREST)
+		img_dict[imageName] = tf.expand_dims(tf.keras.utils.img_to_array(img), 0)
 		return img_dict
